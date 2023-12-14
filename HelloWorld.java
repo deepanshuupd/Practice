b70720438fbbb9d1bcc37f3a47e594ac9ac91c0a
deepanshuupd <upadhayaydeepanshu8@gmail.com>
2023-12-14 11:15:58 +0530

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,3 @@
+public class HelloWorld{ 	static String greeting = "Hello World" ; 	public static void main(String[] args){ 		System.out.println(greeting + "From Deepanshu + "04/12" );
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    static String greeting = "Hello World" ;
+    public static void main(String[] args) {
+
+        System.out.println(greeting + "From Deepanshu" + "04/12" );
+        System.out.println(greeting + args[0]);
+    }
+}
